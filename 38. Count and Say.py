38ba912888c8e63f3cfc88a7197f92043442e651
陈恩丰 <31786202+chenenfeng@users.noreply.github.com>
2018-09-04 13:26:10 -0400

    Create 38. Count and Say.py

---
+++ 38. Count and Say.py
@@ -0,0 +1,24 @@
+#https://leetcode.com/problems/count-and-say/description/
+1
+11
+21
+1211
+111221
+312211
+13112221
+
+# I can't find the rule that numbers make list 
+1
+len(a),a[0]
+j=0
+def count_and_say(j,a):
+    if 
+    for i in range(j, len(a)):
+        # I find something different
+        if a[i]!=a[j]:
+            #count
+            b.append(i-j)
+            #say
+            b.append(a[i])
+    count_and_say(i,a)
+    
